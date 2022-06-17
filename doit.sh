88e6a0f3b8d40c31b43d89715bf1028ad8665b47
Nathaniel Sabanski <sabanski.n@gmail.com>
2022-06-17 04:12:21 -0700

    Updated script.

--- doit.sh
+++ doit.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-set -euo pipefail # Error handling: -e stops the script on errors # -u stops the script unset variables # -o pipefail stops pipelines on command fail: https://mobile.twitter.com/b0rk/status/1314345978963648524
+set -euo pipefail # Error handling: -e stops on errors. -u stops on unset variables. -o pipefail stops pipelines on fail: https://mobile.twitter.com/b0rk/status/1314345978963648524
 
 build() {
   echo "I am ${FUNCNAME[0]}ing"
-  command docker -v || (echo "Error: Docker is not installed"; exit 0) # Check for command.
 }
 
 deploy() {
-  # >/do.sh deploy a b c
+  # >doit deploy a b c
   echo "I am ${FUNCNAME[0]}ing with args '$1 $2 $3'" # I am deploying with Arg 1=a Arg 2=b and Arg 3=c
 }
 
-test() { echo "I am ${FUNCNAME[0]}ing in just one line."; }
+clean() { echo "I am ${FUNCNAME[0]}ing in just one line."; }
 
-_hidden() {
+_preflight() {
   echo "I am a hidden task because I start with _. You can still call me directly"
+  command docker -v || (echo "Error: Docker is not installed"; exit 1) # Check for command.
 }
 
 all() {
-  build && test && deploy
+  clean && build && deploy
 }
 
 "$@" # <- Do it.
