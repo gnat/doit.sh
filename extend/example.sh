e9c62f77d15ad7935be24b97afff926a17309150
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-02-05 12:14:05 -0800

    Example extension.

---
+++ extend/example.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Hello world! You passed $# arguments."
+
+# Nice pattern: Run child scripts!
+
+#{ curl -fsSL https://raw.githubusercontent.com/gnat/doit/main/extend/example.sh | bash; } || echo "Not found."
+#{ curl -fsSL https://raw.githubusercontent.com/gnat/doit/main/extend/example.sh | bash; } || echo "Not found."
