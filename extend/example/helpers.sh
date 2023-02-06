f9d35855f140e69ae2fd8b395302ee7bc5f1c02f
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-02-05 18:08:56 -0800

    Create helpers.sh

---
+++ extend/example/helpers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+
+# Include helpers.
+SCRIPT=$(curl -fsSL https://raw.githubusercontent.com/gnat/doit/main/extend/common.sh) && . "$SCRIPT"
+
+# Run helpers.
+timestamp()
+
+echo "✅"
