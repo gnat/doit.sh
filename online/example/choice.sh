f0fb1eb5c2e0682b3c787f6cfa5897bf4fa1b950
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-02-10 04:24:05 -0800

    Update choice.sh

--- online/example/choice.sh
+++ online/example/choice.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 read -p "❓ Do you want to choose yes or no? (y/n)" CHOICE
 if [[ "$CHOICE" == "y" ]]; then
   echo "YES" 
-elif [[ "$CHOICE" == "n" ]]; then
+else [[ "$CHOICE" == "n" ]]; then
   echo "NO" 
 fi
 
