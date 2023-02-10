b4cc67212983cba22bb55d66c192223e2ee0b240
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-02-10 05:14:10 -0800

    Updated choices example with warning about user input.

---
+++ online/examples/choices.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+# PLEASE NOTE: cURL to bash pipes (curl "..." | bash) will break user input prompts such as read!
+#
+# You'll must load this type of script using one of the alternatives:
+#
+# A) Include.
+# . <(curl -fsSL https://raw.githubusercontent.com/gnat/doit/main/online/$1.sh)
+# 
+# B) Just run.
+# bash --login -s -c "$(curl -fsSL https://raw.githubusercontent.com/gnat/doit/main/online/$1.sh)" -- ${@:2}
+#
+# C) Run, and allow chaining:
+# { _SCRIPT=$(curl -fsSL https://raw.githubusercontent.com/gnat/doit/main/online/$1.sh) && bash --login -s -c "$_SCRIPT" -- ${@:2}; } || echo "Not found!"
+
+read -p "❓ Do you want to choose yes or no? (y/n)" CHOICE
+if [[ "$CHOICE" == "y" ]]; then
+  echo "YES" 
+elif [[ "$CHOICE" == "n" ]]; then
+  echo "NO" 
+fi
+
+echo "Entered: $CHOICE"
+echo "✅"
+
+
