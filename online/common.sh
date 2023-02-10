8fc9e6b442461a42382cd9de82fefe18f5ee0443
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-02-10 03:03:59 -0800

    Update common.sh

--- online/common.sh
+++ online/common.sh
@@ -1,1 +1,4 @@
+# Generate human readable, sortable file timestamp to the second.
+# EXAMPLE=$(timestamp); echo $EXAMPLE;
+# 2021_05_04__10_31_12
 timestamp() { echo "$(date -u +"%Y_%m_%d__%H_%M_%S")"; }
