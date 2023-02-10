ecec56d221953d571c15ff1bba1bcaff143e2694
Nathaniel Sabanski <sabanski.n@gmail.com>
2023-02-10 00:18:20 -0800

    Create install_check.sh

---
+++ extend/docker/install_check.sh
@@ -0,0 +1,3 @@
+which docker || (echo "Error: Docker is not installed"; exit 1)
+which docker-compose || (echo "Error: Docker Compose is not installed"; exit 1)
+which nsenter || (echo "Error: nsenter is not installed"; exit 1)
